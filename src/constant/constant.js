9a129e76956f9f02b7f27301706dff352f2a7c35
aniket <aniket.umbc@gmail.com>
2023-03-20 12:25:49 +0530

    change https

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,5 +1,5 @@
 export const UNIBET_CLIENT = 'https://www.unibet.com/betting#/event/live/';
 export const API_UNI_CDN =
-  'http://api.unicdn.net/v1/feeds/sportsbook/event/live.jsonp?';
+  'https://api.unicdn.net/v1/feeds/sportsbook/event/live.jsonp?';
 export const INITIAL_KEY = 0;
 export const CACHE_EXPIRTY = 120000;
